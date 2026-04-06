ddbadd3c99f451d514f9f70934303917a1bf5386
mzcdouglas54 <mscdouglas44@gmail.com>
2026-04-06 15:54:26 -0600

    Through Values

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-<script>
     function openMenu() {
         document.body.classList.add('menu--open');
     }
     function closeMenu() {
         document.body.classList.remove('menu--open');
     }
-</script>
